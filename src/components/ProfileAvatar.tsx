7812ad6044b5b7f49afceeb4b3f671c3bb45e943
Akbar Nafisa <akbarnj788@gmail.com>
2023-09-25 10:09:04 +0700

    add profile avatar component

---
+++ src/components/ProfileAvatar.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+
+export const ProfileAvatar = ({
+  image = '',
+  name = '',
+  size = '38',
+}: {
+  image: string
+  name: string
+  size: string
+}) => {
+  const fallbackName = name || ''
+  return (
+    <div>
+      <Avatar
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+      >
+        <AvatarImage src={image} alt={fallbackName} />
+        <AvatarFallback>
+          {fallbackName
+            ?.replace('@', '')
+            ?.split(' ')
+            ?.map((n: string) => n[0])
+            ?.join('')
+            ?.substring(2, 0)
+            ?.toUpperCase()}
+        </AvatarFallback>
+      </Avatar>
+    </div>
+  )
+}
