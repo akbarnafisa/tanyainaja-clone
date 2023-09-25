fe224c970e737498aef05d8795eeb3f3af8184cc
Akbar Nafisa <akbarnj788@gmail.com>
2023-09-25 08:12:04 +0700

    update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function addDays(date: string, days: number) {
+  const result = new Date(date)
+  result.setDate(result.getDate() + days)
+
+  return result
+}
